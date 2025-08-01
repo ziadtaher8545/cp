0b26da90861867fd1dbfeb34489ff219d5cdf04c
Ziad taher <ziadtaherb@gmail.com>
2025-08-02 02:09:45 +0300

    Update arthimetic_sequnce.cpp

--- arthimetic_sequnce.cpp
+++ arthimetic_sequnce.cpp
@@ -4,13 +4,4 @@ a = ((a + x - 1) / x) * x;
 b = (b / x) * x;
 return (b + a) * (b - a + x) / (2 * x);
 }
-4.9 Sum Range Divisors
-// calc 1e9 in 42ms,can calc more but need big integer
-ll sumRangeDivisors(ll x) {
-ll ans = 0, left = 1, right;
-for (; left <= x; left = right + 1) {
-right = x / (x / left);
-ans += (x / left) * (left + right) * (right - left + 1) / 2;
-}
-return ans;
-}
+
