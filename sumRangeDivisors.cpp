d891f48d7ebe85f133e843254f7cea6f0acc62e1
Ziad taher <ziadtaherb@gmail.com>
2025-08-02 02:16:38 +0300

    Update sumRangeDivisors.cpp

--- sumRangeDivisors.cpp
+++ sumRangeDivisors.cpp
@@ -1,5 +1,8 @@
 
 // calc 1e9 in 42ms,can calc more but need big integer
+/*
+sum(x/1*1+x/2*2+x/3*3+x/4*4......)
+  */
 ll sumRangeDivisors(ll x) {
 ll ans = 0, left = 1, right;
 for (; left <= x; left = right + 1) {
